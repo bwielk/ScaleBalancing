89f6513eb84b0845c6494f5a18418b9145b4e23a
bwielk <bwielk@gmail.com>
2018-01-26 00:22:24 +0100

    All test pass. Implement more tests and try to design more flexible solution

--- src/scale/Scale.java
+++ src/scale/Scale.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Scale {
 
 	public String weigh(int[] scaleWeights, ArrayList<Integer> weights) {
-		String[] extraWeights = new String[2];
+		ArrayList<String> extraWeights = new ArrayList<String>();
 		int difference;
 		if(scaleWeights[0]>scaleWeights[1]){
 			difference = scaleWeights[0] - scaleWeights[1];
@@ -17,11 +17,12 @@ public class Scale {
 		int count = 0;
 			for(int i=0; i<weights.size(); i++){
 				if(weights.get(i) <= difference){
-					extraWeights[count] = String.valueOf(weights.get(i));
+					extraWeights.add(String.valueOf(weights.get(i)));
 					difference -= weights.get(i);
 					count++;
 			}
 		}
+		System.out.println("\n count : " + count);
 		if(count <= 2){
 			return String.join(",", extraWeights);
 		}else{
